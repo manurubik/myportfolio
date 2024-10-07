82c9ba329fa24a0aef1e0704bf7a8c24a193ed1d
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-10-07 16:57:18 +0200

    Fixing minor bugs

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -111,127 +111,132 @@ Me interesan tus conocimientos en "${service}"!!`
   };
 
   return (
-    <Suspense>
-      <motion.section
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-        }}
-        className="m-5"
-      >
-        <div className="container">
-          <div className="flex flex-col lg:flex-row gap-[30px]">
-            <div className="order-2 lg:order-none">
-              <form
-                className="flex flex-col gap-6 p-6 bg-[#27272c] rounded-lg justify-center items-center"
-                onSubmit={sendMessage}
-              >
-                <h3 className="text-4xl text-accent">Contacta conmigo</h3>
-                <p className="text-white/60">
-                  Si deseas discutir un proyecto, colaborar en algo interesante
-                  o simplemente conectar, no dudes en ponerte en contacto
-                  conmigo. Estoy aquí para ayudarte y explorar cómo podemos
-                  trabajar juntos para lograr grandes resultados.
-                </p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
-                  <Input
-                    type="text"
-                    name="firstname"
-                    placeholder="Nombre"
-                    value={formData.firstname}
-                    onChange={handleChange}
-                    required
-                  />
-                  <Input
-                    type="text"
-                    name="lastname"
-                    placeholder="Apellidos"
-                    value={formData.lastname}
-                    onChange={handleChange}
-                    required
-                  />
-                  <Input
-                    type="email"
-                    name="email"
-                    placeholder="(Email)"
-                    value={formData.email}
-                    onChange={handleChange}
-                  />
-                  <Input
-                    type="tel"
-                    name="phone"
-                    placeholder="(Nº de telefono)"
-                    value={formData.phone}
-                    onChange={handleChange}
-                  />
-                </div>
-                <Select
-                  value={formData.service}
-                  onValueChange={handleSelectChange}
-                  required
-                >
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Selecciona un servicio" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Selecciona un servicio</SelectLabel>
-                      <SelectItem value="Desarrollo Web">
-                        Desarrollo Web
-                      </SelectItem>
-                      <SelectItem value="Diseño UI/UX">Diseño UI/UX</SelectItem>
-                      <SelectItem value="Diseño de Logotipos">
-                        Diseño de Logotipos
-                      </SelectItem>
-                      <SelectItem value="Aplicaciones Android">
-                        Aplicaciones Android
-                      </SelectItem>
-                      <SelectItem value="Otros">Otros</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-                <Textarea
-                  className="h-[130px]"
-                  name="message"
-                  placeholder="Escribe aquí tu mensaje."
-                  value={formData.message}
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="m-5"
+    >
+      <div className="container">
+        <div className="flex flex-col lg:flex-row gap-[30px]">
+          <div className="order-2 lg:order-none">
+            <form
+              className="flex flex-col gap-6 p-6 bg-[#27272c] rounded-lg justify-center items-center"
+              onSubmit={sendMessage}
+            >
+              <h3 className="text-4xl text-accent">Contacta conmigo</h3>
+              <p className="text-white/60">
+                Si deseas discutir un proyecto, colaborar en algo interesante o
+                simplemente conectar, no dudes en ponerte en contacto conmigo.
+                Estoy aquí para ayudarte y explorar cómo podemos trabajar juntos
+                para lograr grandes resultados.
+              </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+                <Input
+                  type="text"
+                  name="firstname"
+                  placeholder="Nombre"
+                  value={formData.firstname}
                   onChange={handleChange}
                   required
                 />
-                <Button size="md" className="max-w-40" type="submit">
-                  Enviar mensaje
-                </Button>
-              </form>
-            </div>
-            <div className="flex items-center order-1 lg:order-none mb-8 lg:mb-0 max-lg:text-center max-lg:justify-center lg:w-1/2">
-              <ul className="flex flex-col gap-10">
-                {info.map((item, index) => {
-                  return (
-                    <Link href={item.link} target="_blank" key={index}>
-                      <li className="flex items-center gap-6 group">
-                        <div className="w-[52px] h-[52px] lg:w-[72px] lg:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center group-hover:border group-hover:border-accent">
-                          <div className="text-3xl group-hover:scale-150 max-lg:group-hover:scale-125 transition-all">
-                            {item.icon}
-                          </div>
+                <Input
+                  type="text"
+                  name="lastname"
+                  placeholder="Apellidos"
+                  value={formData.lastname}
+                  onChange={handleChange}
+                  required
+                />
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="(Email)"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                <Input
+                  type="tel"
+                  name="phone"
+                  placeholder="(Nº de telefono)"
+                  value={formData.phone}
+                  onChange={handleChange}
+                />
+              </div>
+              <Select
+                value={formData.service}
+                onValueChange={handleSelectChange}
+                required
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Selecciona un servicio" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Selecciona un servicio</SelectLabel>
+                    <SelectItem value="Desarrollo Web">
+                      Desarrollo Web
+                    </SelectItem>
+                    <SelectItem value="Diseño UI/UX">Diseño UI/UX</SelectItem>
+                    <SelectItem value="Diseño de Logotipos">
+                      Diseño de Logotipos
+                    </SelectItem>
+                    <SelectItem value="Aplicaciones Android">
+                      Aplicaciones Android
+                    </SelectItem>
+                    <SelectItem value="Otros">Otros</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea
+                className="h-[130px]"
+                name="message"
+                placeholder="Escribe aquí tu mensaje."
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+              <Button size="md" className="max-w-40" type="submit">
+                Enviar mensaje
+              </Button>
+            </form>
+          </div>
+          <div className="flex items-center order-1 lg:order-none mb-8 lg:mb-0 max-lg:text-center max-lg:justify-center lg:w-1/2">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <Link href={item.link} target="_blank" key={index}>
+                    <li className="flex items-center gap-6 group">
+                      <div className="w-[52px] h-[52px] lg:w-[72px] lg:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center group-hover:border group-hover:border-accent">
+                        <div className="text-3xl group-hover:scale-150 max-lg:group-hover:scale-125 transition-all">
+                          {item.icon}
                         </div>
-                        <div className="flex-1">
-                          <p className="text-white/60">{item.title}</p>
-                          <h3 className="text-xl group-hover:text-accent">
-                            {item.description}
-                          </h3>
-                        </div>
-                      </li>
-                    </Link>
-                  );
-                })}
-              </ul>
-            </div>
+                      </div>
+                      <div className="flex-1">
+                        <p className="text-white/60">{item.title}</p>
+                        <h3 className="text-xl group-hover:text-accent">
+                          {item.description}
+                        </h3>
+                      </div>
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
           </div>
         </div>
-      </motion.section>
+      </div>
+    </motion.section>
+  );
+};
+const Contacto = () => {
+  return (
+    <Suspense>
+      <Contact />
     </Suspense>
   );
 };
 
-export default Contact;
+export default Contacto;

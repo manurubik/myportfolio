738bae6c8e5a9f6560f56e595138f7fb3dba710b
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-10-07 16:47:50 +0200

    Deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  assetPrefix: isProd ? "/myportfolio" : "",
+  basePath: isProd ? "/myportfolio" : "",
+  trailingSlash: true,
+};
 
 export default nextConfig;

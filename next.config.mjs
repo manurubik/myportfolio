0caf6c1719bf2cae8a41dfbbef18e9821cbbbe3d
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-10-07 17:01:04 +0200

    Fixing minor bugs v2

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,13 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  output: "export",
   assetPrefix: isProd ? "/myportfolio" : "",
   basePath: isProd ? "/myportfolio" : "",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;

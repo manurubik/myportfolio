ce97e0e9efd653490b218629fb29bdea377cdf7e
Manuel Prieto de Antón <manuelprietodeanton@gmail.com>
2024-10-22 02:59:51 +0200

    Add projects + fix URLs

--- app/data/projects.js
+++ app/data/projects.js
@@ -2,14 +2,17 @@ export const projects = [
   {
     num: "01",
     category: "frontend",
-    title: "Generador de Acreditaciones Deportivas",
+    title: "SaaS Landing Page",
     description:
-      "Generador de acreditaciones para eventos deportivos, obteniendo la lista de competidores desde un archivo CSV.",
-    stack: [{ name: "HTML 5" }, { name: "SASS" }, { name: "JavaScript" }],
-    image:
-      "https://manurubik.github.io/myportfolio/assets/work/generador-acreditaciones.png",
-    live: "https://manurubik.github.io/accreditation-generator/",
-    github: "https://github.com/manurubik/accreditation-generator",
+      "La página de inicio para Softwares de Servicios (SaaS) ofrece una experiencia intuitiva y atractiva para los usuarios. Con un diseño moderno y responsivo, la página destaca las características clave de nuestro servicio, incluyendo planes de precios flexibles y testimonios de clientes satisfechos.",
+    stack: [
+      { name: "React + Vite" },
+      { name: "Tailwind.css" },
+      { name: "Node.js" },
+    ],
+    image: "https://manurubik.github.io/myportfolio/assets/work/SAAS.png",
+    live: "https://manurubik.github.io/saas-landing-page",
+    github: "https://github.com/manurubik/saas-landing-page",
   },
   {
     num: "02",
@@ -25,12 +28,44 @@ export const projects = [
   {
     num: "03",
     category: "fullstack",
+    title: "Birthday Greeting App",
+    description:
+      "Birthday Greeting App es una aplicación web que genera felicitaciones de cumpleaños personalizada y permite crear mensajes únicos para celebrar ocasiones especiales de manera sencilla y rápida.",
+    stack: [
+      { name: "React + Vite" },
+      { name: "Tailwind.css" },
+      { name: "Node.js" },
+    ],
+    image: "https://manurubik.github.io/myportfolio/assets/work/birthday.png",
+    live: "https://manurubik.github.io/birthday-greeting-app/",
+    github: "https://github.com/manurubik/birthday-greeting-app",
+  },
+  {
+    num: "04",
+    category: "frontend",
+    title: "Generador de Acreditaciones Deportivas",
+    description:
+      "Generador de acreditaciones para eventos deportivos, obteniendo la lista de competidores desde un archivo CSV.",
+    stack: [
+      { name: "HTML 5" },
+      { name: "SASS" },
+      { name: "JavaScript" },
+      { name: "Gulp" },
+    ],
+    image:
+      "https://manurubik.github.io/myportfolio/assets/work/acreditaciones.png",
+    live: "https://manurubik.github.io/accreditation-generator/",
+    github: "https://github.com/manurubik/accreditation-generator",
+  },
+  {
+    num: "05",
+    category: "fullstack",
     title: "Mi Portfolio",
     description: "Portfolio personal de Manuel Prieto de Antón",
     stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
     image:
       "https://manurubik.github.io/myportfolio/assets/work/myportfolio.png",
-    live: "https://manurubik.github.io/myportfolio",
+    live: "https://manurubik.github.io/myportfolio/",
     github: "https://github.com/manurubik/myportfolio",
   },
 ];
--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="https://manurubik.github.io/myportfolio/favicon.ico"
+        />
       </head>
       <body className={`min-h-screen flex flex-col ${jetbrainsMono.variable}`}>
         <Header />
